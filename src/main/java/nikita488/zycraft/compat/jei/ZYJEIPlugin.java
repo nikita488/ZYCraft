775cb51a50abce42554b30bfb3e9bf3d8caa64f4
nikita488 <6237881+nikita488@users.noreply.github.com>
2021-05-19 22:37:01 +0300

    Added Fabricator menu as extra area inside JEI

--- src/main/java/nikita488/zycraft/compat/jei/ZYJEIPlugin.java
+++ src/main/java/nikita488/zycraft/compat/jei/ZYJEIPlugin.java
@@ -3,15 +3,20 @@ package nikita488.zycraft.compat.jei;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.gui.handlers.IGuiContainerHandler;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeTransferRegistration;
+import net.minecraft.client.renderer.Rectangle2d;
 import net.minecraft.util.ResourceLocation;
 import nikita488.zycraft.ZYCraft;
 import nikita488.zycraft.client.gui.screen.inventory.FabricatorScreen;
 import nikita488.zycraft.compat.jei.gui.handlers.FabricatorGhostIngredientHandler;
 import nikita488.zycraft.compat.jei.transfer.FabricatorRecipeTransferHandler;
 import nikita488.zycraft.init.ZYBlocks;
+
+import java.util.Collections;
+import java.util.List;
 
 @JeiPlugin
 public class ZYJEIPlugin implements IModPlugin
@@ -33,6 +38,14 @@ public class ZYJEIPlugin implements IModPlugin
     @Override
     public void registerGuiHandlers(IGuiHandlerRegistration registration)
     {
+        registration.addGuiContainerHandler(FabricatorScreen.class, new IGuiContainerHandler<FabricatorScreen>()
+        {
+            @Override
+            public List<Rectangle2d> getGuiExtraAreas(FabricatorScreen screen)
+            {
+                return Collections.singletonList(new Rectangle2d(screen.getGuiLeft() - 34, screen.getGuiTop() + 23, 34, 80));
+            }
+        });
         registration.addGhostIngredientHandler(FabricatorScreen.class, new FabricatorGhostIngredientHandler());
     }
 
