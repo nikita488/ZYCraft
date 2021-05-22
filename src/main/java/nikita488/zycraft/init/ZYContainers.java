00cf64a9257a43bc49cfd5a39216a7e067e93890
nikita488 <6237881+nikita488@users.noreply.github.com>
2021-05-22 10:05:59 +0300

    Removed redundant cast

--- src/main/java/nikita488/zycraft/init/ZYContainers.java
+++ src/main/java/nikita488/zycraft/init/ZYContainers.java
@@ -1,7 +1,6 @@
 package nikita488.zycraft.init;
 
 import com.tterrag.registrate.Registrate;
-import com.tterrag.registrate.builders.ContainerBuilder;
 import com.tterrag.registrate.util.entry.ContainerEntry;
 import nikita488.zycraft.ZYCraft;
 import nikita488.zycraft.client.gui.screen.inventory.FabricatorScreen;
@@ -11,7 +10,7 @@ public class ZYContainers
 {
     private static final Registrate REGISTRATE = ZYCraft.registrate();
 
-    public static final ContainerEntry<FabricatorContainer> FABRICATOR = REGISTRATE.container("fabricator", (ContainerBuilder.ContainerFactory<FabricatorContainer>)FabricatorContainer::new, () -> FabricatorScreen::new)
+    public static final ContainerEntry<FabricatorContainer> FABRICATOR = REGISTRATE.container("fabricator", FabricatorContainer::new, () -> FabricatorScreen::new)
             .register();
 
     public static void init() {}
